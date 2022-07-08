a01c001c25423578f9adceccb7ae0b9a0006f6d3
Jester <justjestervz@gmail.com>
2022-07-08 22:54:10 +0300

    can add logs

--- Ahed-project/ViewModel/ContentPageViewModel.cs
+++ Ahed-project/ViewModel/ContentPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Ahed_project.Pages;
+﻿using Ahed_project.MasterData;
+using Ahed_project.Pages;
 using Ahed_project.Services;
 using DevExpress.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -15,16 +17,30 @@ namespace Ahed_project.ViewModel
     public class ContentPageViewModel : BindableBase
     {
         private readonly PageService _pageService;
+        private readonly Logs _logs;
 
-        public ContentPageViewModel(PageService pageService)
+        public ObservableCollection<LoggerMessage> LogCollection { get; set; }
+
+        public ContentPageViewModel(PageService pageService, Logs logs)
         {
             _pageService = pageService;
+            _logs = logs;
+            for(int i = 0; i < 10; i++)
+            {
+                _logs.AddMessage("Info", $"Log {i}");
+            }
+            LogCollection = _logs.logs;
+
         }
 
         public ICommand Logout => new AsyncCommand(async () => {
             var assembly = Assembly.GetExecutingAssembly();
             File.Delete(Path.GetDirectoryName(assembly.Location) + "\\Config\\token.txt");
             _pageService.ChangePage(new LoginPage());
+        });
+
+        public ICommand Exit => new DelegateCommand(() => {
+            System.Windows.Application.Current.Shutdown();
         });
     }
 }
