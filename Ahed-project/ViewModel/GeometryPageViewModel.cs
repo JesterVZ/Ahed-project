e59058d8055482ca25bf8068f08c5c348751d439
Jester <justjestervz@gmail.com>
2022-09-02 00:50:59 +0300

    То же самое в geometry page

---
+++ Ahed-project/ViewModel/GeometryPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ahed_project.ViewModel
+{
+    public class GeometryPageViewModel : BindableBase
+    {
+        public bool IsOpen { get; set; }
+
+        #region coms
+
+        public ICommand ToggleCommand => new DelegateCommand(async () =>
+        {
+            IsOpen = !IsOpen;
+        });
+
+        #endregion
+    }
+}
