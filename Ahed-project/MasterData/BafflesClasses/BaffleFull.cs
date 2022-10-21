d6c8808d1ecbec7feff9ff72681d7d75891f62c6
Jester <justjestervz@gmail.com>
2022-10-21 19:57:55 +0500

    add baffle fields

--- Ahed-project/MasterData/BafflesClasses/BaffleFull.cs
+++ Ahed-project/MasterData/BafflesClasses/BaffleFull.cs
@@ -10,6 +10,17 @@ namespace Ahed_project.MasterData.BafflesClasses
     {
         public string type { get; set; }
         public string shell_inner_diameter { get; set; }
+        public string tubes_outer_diameter { get; set; }
+        public string buffle_cut { get; set; }
+        public string baffle_cut_direction { get; set; }
+        public string pairs_of_sealing_strips { get; set; }
+        public string shell_diameter_angle { get; set; }
+        public string center_tube_angle { get; set; }
+        public string diameter_to_tube_center { get; set; }
+        public string diameter_to_tube_outer_side { get; set; }
+        public string bypass_lanes { get; set; }
+        public string inner_shell_to_outer_tube_bypass_clearance { get; set; }
+        public string average_tubes_in_baffle_windows { get; set; }
 
     }
 }
