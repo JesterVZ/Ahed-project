0904743dee07f20ae6a38ff47d33379198a23692
Jester <justjestervz@gmail.com>
2022-10-24 17:33:05 +0500

    baffles правка 7 готова

--- Ahed-project/ViewModel/Pages/BufflesPageViewModel.cs
+++ Ahed-project/ViewModel/Pages/BufflesPageViewModel.cs
@@ -11,17 +11,23 @@ namespace Ahed_project.ViewModel.Pages
     public class BufflesPageViewModel : BindableBase
     {
         public Dictionary<string, string> Type { get; set; }
+        public Dictionary<string, string> BaffleType { get; set; } // No baffles, Standard heat transfer with SUPPORT baffles, Full baffles heat transfer calculation
         public bool IsOpen { get; set; }
         public double SingleSegmentalIsEnables { get; set; }
         public double DoubleSegmentalIsEnables { get; set; }
         public BufflesPageViewModel()
         {
             Type = new Dictionary<string, string>();
+            BaffleType = new Dictionary<string, string>();
 
             Type.Add("single_segmental", "Single Segmental");
             Type.Add("double_segmental", "Double Segmental");
-        }
 
+            BaffleType.Add("no_baffles", "No baffles");
+            BaffleType.Add("standard_heat_transfer_with_support_baffles", "Standard heat transfer with SUPPORT baffles");
+            BaffleType.Add("full_baffles_heat_transfer_calculation", "Full baffles heat transfer calculation");
+        }
+        #region KeyValuePairs
         private KeyValuePair<string, string> _selectedType;
         public KeyValuePair<string, string> SelectedType
         {
@@ -42,11 +48,23 @@ namespace Ahed_project.ViewModel.Pages
             }
         }
 
+        private KeyValuePair<string, string> _selectedBaffleType;
+        public KeyValuePair<string, string> SelectedBaffleType
+        {
+            get => _selectedBaffleType;
+            set
+            {
+                _selectedBaffleType = value;
+            }
+        }
+        #endregion
+
+        #region commands
         public ICommand ToggleCommand => new DelegateCommand(async () =>
         {
             IsOpen = !IsOpen;
 
         });
-
+        #endregion
     }
 }
