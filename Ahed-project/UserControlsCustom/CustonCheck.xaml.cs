8aaed3686f80be4f13ca3d8a91b5fa17ac9618aa
Jester <justjestervz@gmail.com>
2022-12-01 19:30:59 +0500

    try to register DependencyProperty for custom checkbox

--- Ahed-project/UserControlsCustom/CustonCheck.xaml.cs
+++ Ahed-project/UserControlsCustom/CustonCheck.xaml.cs
@@ -20,6 +20,13 @@ namespace Ahed_project.UserControlsCustom
     /// </summary>
     public partial class CustonCheck : System.Windows.Controls.UserControl
     {
+        private string _text;
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(CustonCheck), new PropertyMetadata(false, new PropertyChangedCallback(ChangeTextProperty)));
         public CustonCheck()
         {
             InitializeComponent();
@@ -27,14 +34,19 @@ namespace Ahed_project.UserControlsCustom
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            CustomTextBox.IsEnabled = false;
-            CustomTextBox.Background = new SolidColorBrush(Color.FromRgb(249, 239, 229));
+            CustomTextBox.IsEnabled = true;
+            CustomTextBox.Background = new SolidColorBrush(Colors.White);
+            //CustomTextBox.Background = new SolidColorBrush(Color.FromRgb(249, 239, 229));
         }
 
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            CustomTextBox.IsEnabled = true;
-            CustomTextBox.Background = new SolidColorBrush(Colors.White);
+            CustomTextBox.IsEnabled = false;
+            CustomTextBox.Background = new SolidColorBrush(Color.FromRgb(249, 239, 229));
+        }
+        private static void ChangeTextProperty(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as CustonCheck).Text = (string)e.NewValue;
         }
     }
 }
