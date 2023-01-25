ce28dd5dcac91f8195bcda279464cb27a19e4d44
Александр Вахрин <vakhrin@i-teco.ru>
2023-01-25 11:58:38 +0500

    fix

--- Ahed-project/MasterData/Converter.cs
+++ Ahed-project/MasterData/Converter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Ahed_project.MasterData
@@ -11,6 +12,10 @@ namespace Ahed_project.MasterData
     {
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            if (values[0] == DependencyProperty.UnsetValue)
+            {
+                return null;
+            }
             string value = System.Convert.ToDecimal(values[0]?.ToString()?.Replace('.',',')).ToString($"F{Config.NumberOfDecimals}");
             return value;
         }
