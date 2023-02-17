ed1247dfd3a78c50bcfc2784f1c404b267331704
Vladimir Zhulin <45514323+JesterVZ@users.noreply.github.com>
2023-02-17 16:36:26 +0500

    единицы измерения в геометрии

---
+++ Ahed-project/UserControlsCustom/CustomTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ahed_project.UserControlsCustom
+{
+    public class CustomTextBox : TextBox
+    {
+        public static readonly DependencyProperty dependencyProperty = DependencyProperty.Register("IsError", typeof(bool), typeof(TextBox), new PropertyMetadata(false, new PropertyChangedCallback(ChangeErrorProperty)));
+
+        public bool IsError
+        {
+            get => (bool)GetValue(dependencyProperty);
+            set => SetValue(dependencyProperty, value);
+        }
+
+        private static void ChangeErrorProperty(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            (d as CustomTextBox).IsError = (bool)e.NewValue;
+        }
+    }
+}
